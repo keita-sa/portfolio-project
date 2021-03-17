58d16f99b9860ea105f8b6431cbd58cf45dbc6b3
桜井啓太 <keitasakuraims@gmail.com>
2021-03-17 09:31:09 +0900

    Jobs app up and running

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-# Add to the admin
+    def __str__(self):
+        return self.titel
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
